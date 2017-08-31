f41abc6bc9f775ac57feefc0fd141f0a5b68c088
Rodolfo Gomes <SilvaR@fbd.ie>
2017-08-31 11:27:25 +0100

    Teste

---
+++ wmpweb/src/main/java/ie/com/wmp/wmpweb/App.java
@@ -0,0 +1,13 @@
+package ie.com.wmp.wmpweb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
